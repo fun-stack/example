dc7b90941adbc8833a7a60f99927b8957ae9d735
johannes karoff <johannes@karoff.net>
2022-01-18 17:03:22 +0100

    update fun-stack

---
+++ src/main/g8/terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "url_website" {
+  value = module.$name;format="lower,snake"$.url_website
+}
+
+output "url_http" {
+  value = module.$name;format="lower,snake"$.url_http
+}
+
+output "url_ws" {
+  value = module.$name;format="lower,snake"$.url_ws
+}
+
+output "url_auth" {
+  value = module.$name;format="lower,snake"$.url_auth
+}
