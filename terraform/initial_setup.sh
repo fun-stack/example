b082a627bf9d650563f6c525d2fca793db893fd1
johannes karoff <johannes@karoff.net>
2022-01-30 20:06:11 +0100

    make initial setup reentrant

--- terraform/initial_setup.sh
+++ terraform/initial_setup.sh
@@ -9,11 +9,11 @@ terraform_state_bucket="${account}-terraform-state"
 terraform_state_lock_table="terraform-lock"
 
 echo "Create s3 bucket for terraform state: ${terraform_state_bucket}"
-aws s3 mb s3://${terraform_state_bucket}
-aws s3api put-bucket-versioning --bucket ${terraform_state_bucket} --versioning-configuration Status=Enabled
+aws s3 mb s3://${terraform_state_bucket} || true
+aws s3api put-bucket-versioning --bucket ${terraform_state_bucket} --versioning-configuration Status=Enabled || true
 
 echo "Create dynamodb table for terraform state lock: ${terraform_state_lock_table}"
-aws dynamodb create-table --table-name ${terraform_state_lock_table} --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST > /dev/null
+aws dynamodb create-table --table-name ${terraform_state_lock_table} --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST > /dev/null || true
 
 cat <<EOF > terraform.tf
 terraform {
