facb0f4661960475b4ae48a0e00fa5517feec667
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-03-22 20:44:26 -0600

    Implement generic ApiRequestLogger using Functor

--- lambda/src/main/scala/example/lambda/ApiRequestLogger.scala
+++ lambda/src/main/scala/example/lambda/ApiRequestLogger.scala
@@ -1,51 +1,20 @@
 package example.lambda
 
-import funstack.lambda.{http, ws}
+import cats.Functor
+import cats.implicits._
 import sloth.LogHandler
 
 import scala.scalajs.js
 
-//class ApiRequestLogger[X[_]](implicit ev: X[*] =:= Kleisli[IO, RequestOf[Any], *]) extends LogHandler[X] {
-//  implicit val executionContext = org.scalajs.macrotaskexecutor.MacrotaskExecutor
-//  def logRequest[ARG, RES](
-//    path: List[String],
-//    argumentObject: ARG,
-//    result: X[RES],
-//  ): X[RES] = {
-//    println(s"-> ${fansi.Color.Yellow(path.mkString("."))}(${argumentObject})")
-//    result.tapWith { (_, res) =>
-//      print(s"<- ")
-//      pprint.pprintln(res)
-//      res
-//    }
-//  }
-//}
-
-object HttpApiRequestLogger extends LogHandler[http.rpc.Handler.IOKleisli] {
+class ApiRequestLogger[F[_]: Functor] extends LogHandler[F] {
   def logRequest[ARG, RES](
     path: List[String],
     argumentObject: ARG,
-    result: http.rpc.Handler.IOKleisli[RES],
-  ): http.rpc.Handler.IOKleisli[RES] = {
+    result: F[RES],
+  ): F[RES] = {
     val args = if (argumentObject.asInstanceOf[js.UndefOr[_]] == js.undefined) "" else argumentObject
     println(s"-> ${fansi.Color.Yellow(path.mkString("."))}($args)")
-    result.tapWith { (_, res) =>
-      print(s"<- ")
-      pprint.pprintln(res)
-      res
-    }
-  }
-}
-
-object WsApiRequestLogger extends LogHandler[ws.rpc.Handler.IOKleisli] {
-  def logRequest[ARG, RES](
-    path: List[String],
-    argumentObject: ARG,
-    result: ws.rpc.Handler.IOKleisli[RES],
-  ): ws.rpc.Handler.IOKleisli[RES] = {
-    val args = if (argumentObject.asInstanceOf[js.UndefOr[_]] == js.undefined) "" else argumentObject
-    println(s"-> ${fansi.Color.Yellow(path.mkString("."))}($args)")
-    result.tapWith { (_, res) =>
+    result.map { res =>
       print(s"<- ")
       pprint.pprintln(res)
       res
