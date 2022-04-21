84f6c2a280687bdf2813528418a83e7d3c9620a1
johannes karoff <johannes@karoff.net>
2022-04-21 10:22:12 +0200

    formatting

--- webapp/src/main/scala/example/webapp/Page.scala
+++ webapp/src/main/scala/example/webapp/Page.scala
@@ -8,22 +8,22 @@ sealed trait Page {
 }
 
 object Page {
-  case object Home      extends Page
-  case object Api      extends Page
+  case object Home extends Page
+  case object Api  extends Page
 
   object Paths {
-    val Home      = Root
-    val Api      = Root / "api"
+    val Home = Root
+    val Api  = Root / "api"
   }
 
   val fromPath: Path => Page = {
-    case Paths.Api      => Page.Api
-    case _               => Page.Home
+    case Paths.Api => Page.Api
+    case _         => Page.Home
   }
 
   val toPath: Page => Path = {
-    case Page.Api      => Paths.Api
-    case Page.Home      => Paths.Home
+    case Page.Api  => Paths.Api
+    case Page.Home => Paths.Home
   }
 
   val current: Subject[Page] = Router.path
