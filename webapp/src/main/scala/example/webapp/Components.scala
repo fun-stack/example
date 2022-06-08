0506ea64e2b0a10d7c12056d71d59ad1023c1f0d
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-06-07 18:52:21 -0600

    Refactor frontend

--- webapp/src/main/scala/example/webapp/Components.scala
+++ webapp/src/main/scala/example/webapp/Components.scala
@@ -36,26 +36,87 @@ object Components {
   }
 
   val httpApi = div(
-    cls := "text-lg",
-    "Http",
+    h2("Http Api", cls := "text-xl"),
     div(
       // openapi with tapir
       b("My books: "),
-      tapir.Fun.http
-        .client(HttpApi.booksListing)((HttpApi.BooksFromYear("drama", 2011), 10))
-        .map(_.toString),
+      span(
+        cls := "tapir-result",
+        tapir.Fun.http
+          .client(HttpApi.booksListing)((HttpApi.BooksFromYear("drama", 2011), 10))
+          .map(_.toString),
+      ),
     ),
-    renderApi(HttpClient.api),
   )
 
-  val websocketApi = div(
-    cls := "text-lg",
-    "Ws",
-    renderApi(WsClient.api),
+  def httpRpcApi = {
+    val currentRandomNumber = Subject.behavior[Option[Int]](None)
+
     div(
-      // incoming events from the websocket
-      b("Events (ws):"),
-      WsClient.streamsApi.logs.map(div(_)).scanToList,
-    ),
-  )
+      h2("Http Rpc Api", cls := "text-xl"),
+      div(
+        // example of rendering an async call directly
+        // https://outwatch.github.io/docs/readme.html#rendering-futures
+        // https://outwatch.github.io/docs/readme.html#rendering-async-effects
+        b("Number to string via api call: "),
+        span(HttpClient.api.numberToString(3), cls := "http-rpc-number-to-string"),
+      ),
+      div(
+        // example of dynamic content with EmitterBuilder (onClick), IO (asEffect), and Subject/Observable/Observer (currentRandomNumber)
+        // https://outwatch.github.io/docs/readme.html#dynamic-content
+        div(
+          b("Current random number: "),
+          span(currentRandomNumber),
+        ),
+        button(
+          "Get New Random Number from API",
+          onClick.asEffect(HttpClient.api.getRandomNumber).map(Some.apply) --> currentRandomNumber,
+          cls := "btn btn-primary btn-sm",
+        ),
+      ),
+    )
+  }
+
+  def websocketRpcApi = {
+    val currentRandomNumber = Subject.behavior[Option[Int]](None)
+
+    div(
+      h2("Websocket Rpc Api", cls := "text-xl"),
+      div(
+        // example of rendering an async call directly
+        // https://outwatch.github.io/docs/readme.html#rendering-futures
+        // https://outwatch.github.io/docs/readme.html#rendering-async-effects
+        b("Number to string via api call: "),
+        span(WsClient.api.numberToString(3), cls := "websocket-rpc-number-to-string"),
+      ),
+      div(
+        // example of dynamic content with EmitterBuilder (onClick), IO (asEffect), and Subject/Observable/Observer (currentRandomNumber)
+        // https://outwatch.github.io/docs/readme.html#dynamic-content
+        div(
+          b("Current random number: "),
+          span(currentRandomNumber),
+        ),
+        button(
+          "Get New Random Number from API",
+          onClick.asEffect(WsClient.api.getRandomNumber).map(Some.apply) --> currentRandomNumber,
+          cls := "btn btn-primary btn-sm",
+          cls := "websocket-rpc-new-random-number-button",
+        ),
+      ),
+    )
+  }
+
+  def websocketEvents =
+    div(
+      h2("Websocket Events", cls := "text-xl"),
+      div(
+        // incoming events from the websocket
+        div("(press random number button)", cls := "text-gray-500"),
+        div(
+          WsClient.streamsApi.logs.map(div(_)).scanToList,
+          cls                                   := "websocket-event-list",
+        ),
+      ),
+    )
+
 }
