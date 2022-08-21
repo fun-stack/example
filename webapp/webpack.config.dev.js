299fedd156a624c22e3dcbe1992f7ecbcd82e763
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-08-22 00:48:08 +0200

    fix webpack

--- webapp/webpack.config.dev.js
+++ webapp/webpack.config.dev.js
@@ -2,13 +2,12 @@ const { webDev } = require("@fun-stack/fun-pack"); // https://github.com/fun-sta
 
 // https://github.com/fun-stack/fun-pack
 module.exports = webDev({
-  // commented out because CopyWebpackPlugin is defined here to exclude the sprite files.
-  // indexHtml: "src/main/html/index.html",
+  indexHtml: "src/main/html/index.html",
   assetsDir: "assets",
   extraWatchDirs: [
     "local", // frontend with local backend
     "src/css",
-    // "../terraform/.terraform/modules/nocode/serve/" // frontend with deployed backend
+    // "../terraform/.terraform/modules/example/serve/" // frontend with deployed backend
   ],
   extraStaticDirs: [
     "src", // src for source maps
